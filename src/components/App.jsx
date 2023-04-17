6c793e4607760f9b2f30c4723e382790603dd3f2
Oleg <112899765+OlegSyrovatko@users.noreply.github.com>
2023-04-17 12:04:05 +0300

    default phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,68 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactList from 'components/ContactList';
+import Filter from 'components/Filter';
+import ContactForm from 'components/ContactForm';
+import { Book } from './App.styled';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const isNameExists = this.state.contacts.some(
+      contact => contact.name === name
+    );
+    if (isNameExists) {
+      return alert(`${name} is already in contacts`);
+    }
+    this.setState(({ contacts }) => ({
+      contacts: [{ id: nanoid(), name, number }, ...contacts],
+    }));
+  };
+
+  handleChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  filterContacts = () => {
+    const { filter, contacts } = this.state;
+    const lowerFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(lowerFilter)
+    );
+  };
+  onDeleteContact = id => {
+    this.setState({
+      contacts: this.state.contacts.filter(contact => contact.id !== id),
+    });
+  };
+
+  render() {
+    const { filter } = this.state;
+    const visibleContacts = this.filterContacts();
+    return (
+      <Book>
+        <ContactForm onSubmit={this.addContact} />
+        {this.state.contacts.length > 0 && (
+          <>
+            <h2>Contacts</h2>
+            <Filter value={filter} onChange={this.handleChange} />
+          </>
+        )}
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteContact={this.onDeleteContact}
+        />
+      </Book>
+    );
+  }
+}
